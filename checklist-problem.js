2584a6aed312010162fae2642c742c28b6f97d61
Rabiullah-Rabi <mdrabiullah789@gmail.com>
2022-08-03 00:37:00 +0600

    checklist 24 solve

---
+++ checklist-problem.js
@@ -0,0 +1,17 @@
+var moneyHave = 120000;
+
+if(moneyHave >= 80000){
+    console.log('mac kinbo ');
+}
+else if(moneyHave >= 60000 && moneyHave < 80000 ){
+    console.log('GAmming laptop kinbo ');
+}
+else if(moneyHave >= 40000 && moneyHave < 60000 ){
+    console.log('Lenovo yoga  kinbo ');
+}
+else if(moneyHave >= 20000){
+    console.log('Old PC  kinbo ');
+}
+else{
+    console.log('kichu kina lagbe na tor. Mobile ache na? Oita diya kaaj chala ');
+}
