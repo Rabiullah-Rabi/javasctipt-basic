dd5d39db37ae90a0a79ed66e596b938947e05242
Rabiullah-Rabi <mdrabiullah789@gmail.com>
2022-08-03 01:15:16 +0600

    checklist 34

--- checklist-33.js
+++ checklist-33.js
@@ -1,3 +1,5 @@
+// তুমি এতদিন যা যা জিনিস শিখছো সেগুলার নাম দিয়ে একটা array বানাও। তারপর একটা for লুপ দিয়ে সেই array এর সব উপাদান কে আউটপুট হিসেবে দেখাও। 
+
 var learned = ['HTML' , 'CSS' , 'Bootstrap' , 'Tailwind' , 'Basic JS'];
 for(let i = 0; i < learned.length; i++){
     console.log(learned[i]);
