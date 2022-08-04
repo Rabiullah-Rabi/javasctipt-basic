df3d8d0990f90d50d8208e99abdbec6c669d8881
Rabiullah-Rabi <mdrabiullah789@gmail.com>
2022-08-04 20:46:33 +0600

    even odd

---
+++ even-odd.js
@@ -0,0 +1,10 @@
+function odd_even(number) {
+    if (number % 2 == 0) {
+        console.log(number + ' is even');
+    }
+    else {
+        console.log(number + ' is Odd');
+    }
+
+}
+odd_even(1025);
