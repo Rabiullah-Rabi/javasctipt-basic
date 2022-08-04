cc816946c21766b76944894b7676669ff53ae306
Rabiullah-Rabi <mdrabiullah789@gmail.com>
2022-08-04 19:02:49 +0600

    function to make avarage of 3 numbers

---
+++ avarage.js
@@ -0,0 +1,9 @@
+function make_avg(num1, num2, num3) {
+    var total = num1 + num2 + num3;
+    var avg = total / 3;
+    return avg;
+}
+var num1 = 10;
+var num2 = 15;
+var num3 = 50;
+console.log(make_avg(num1, num2, num3));
