c02a383019f675e2d62c00c23c4b3ae618bbdc35
Rabiullah-Rabi <mdrabiullah789@gmail.com>
2022-08-07 13:04:19 +0600

    some basic string operation

---
+++ string-operation-1.js
@@ -0,0 +1,13 @@
+const str = "have Fun";
+console.log(str.length);
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+console.log(str.toLowerCase().includes('Fun'.toLowerCase()));
+console.log(str.indexOf('u'));
+console.log(str.startsWith('fun'));
+console.log(str.endsWith('Fun'));
+
+const file = 'check.pdf';
+console.log(file.endsWith('.pdf'));
+
+
